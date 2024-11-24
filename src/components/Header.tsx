972f879cce58f4720f60a25f3ce6893fdd595d9d
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-11-25 00:27:23 +0530

    added hero sec

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,74 @@
+"use client";
+import { AlignRight, X } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const menuhandler = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <div>
-      <div className="flex justify-between px-10 py-6 border-b">
-        <h1 className="text-2xl font-semibold">psycheMaster</h1>
-        <nav>
-          <ul className="flex gap-8 items-center">
-            <Link href="/">
-              <li className="cursor-pointer text-xl font-normal">Home</li>
-            </Link>
-            <Link href="/about">
-              <li className="cursor-pointer text-xl font-normal">About</li>
-            </Link>
-            <Link href="/services">
-              <li className="cursor-pointer text-xl font-normal">Services</li>
-            </Link>
-            <Link href="/contact">
-              <li className="cursor-pointer text-xl font-normal">Contact</li>
-            </Link>
-          </ul>
-        </nav>
-        <button className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded">
-          Book Appointment
-        </button>
+    <div className="sticky top-0 bg-pink-50 z-20">
+      <div className="hidden md:block">
+        <div className="flex justify-between px-10 py-6 border-b">
+          <h1 className="text-2xl font-semibold">psycheMaster</h1>
+          <nav>
+            <ul className="flex gap-8 items-center">
+              <Link href="/">
+                <li className="cursor-pointer text-xl font-normal">Home</li>
+              </Link>
+              <Link href="/about">
+                <li className="cursor-pointer text-xl font-normal">About</li>
+              </Link>
+              <Link href="/services">
+                <li className="cursor-pointer text-xl font-normal">Services</li>
+              </Link>
+              <Link href="/contact">
+                <li className="cursor-pointer text-xl font-normal">Contact</li>
+              </Link>
+            </ul>
+          </nav>
+          <button className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded">
+            Book Appointment
+          </button>
+        </div>
+      </div>
+
+      <div className="md:hidden">
+        <div className="flex py-5 justify-between px-6 border-b">
+          <h1 className="text-2xl font-semibold">psycheMaster</h1>
+          <div className="bg-pink-500 rounded-xl p-2">
+            {showMenu ? (
+              <X onClick={menuhandler} />
+            ) : (
+              <AlignRight onClick={menuhandler} />
+            )}
+          </div>
+        </div>
+        {showMenu && (
+          <nav className="flex items-center flex-col gap-10 mb-10">
+            <ul className="flex flex-col mt-10 gap-8 items-center">
+              <Link href="/">
+                <li className="cursor-pointer text-xl font-normal">Home</li>
+              </Link>
+              <Link href="/about">
+                <li className="cursor-pointer text-xl font-normal">About</li>
+              </Link>
+              <Link href="/services">
+                <li className="cursor-pointer text-xl font-normal">Services</li>
+              </Link>
+              <Link href="/contact">
+                <li className="cursor-pointer text-xl font-normal">Contact</li>
+              </Link>
+            </ul>
+            <button className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded">
+              Book Appointment
+            </button>
+          </nav>
+        )}
       </div>
     </div>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,67 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { GridPattern } from "./ui/grid-pattern";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 export default function Hero() {
   return (
-    <div className="relative flex h-screen items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl">
-      <h2>Welcome to PsycheMaster</h2>
-      <GridPattern
-        width={40}
-        height={40}
-        x={-1}
-        y={-1}
-        squares={[
-          [4, 4],
-          [5, 1],
-          [8, 2],
-          [5, 3],
-          [5, 5],
-        ]}
-        className={cn(
-          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
-        )}
-      />
+    <div className="relative flex  md:h-screen h-[70svh]  overflow-hidden rounded-lg border bg-background md:shadow-xl">
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 12000, disableOnInteraction: false }}
+        loop
+      >
+        <SwiperSlide>
+          <div
+            className="w-full h-screen bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/hero-bg1.jpg')" }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-full h-screen bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/hero-bg2.jpg')" }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-full h-screen bg-cover bg-center"
+            style={{ backgroundImage: "url('/images/hero-bg3.jpg')" }}
+          ></div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="relative w-full h-full overflow-hidden">
+        {/* Background Images */}
+        <div className="absolute inset-0 bg-cover bg-center animate-slide1"></div>
+        <div className="absolute inset-0 bg-cover bg-center animate-slide2"></div>
+        <div className="absolute inset-0 bg-cover bg-center animate-slide3"></div>
+
+        <div className="absolute inset-0 bg-black opacity-70"></div>
+
+        {/* Content */}
+        <div className="relative z-10 gap-6 flex flex-col items-center justify-center h-full text-white">
+          <h3 className="text-2xl font-sans text-pink-50  font-semibold">
+            --- welcome to psycheMaster
+          </h3>
+          <h1 className="text-4xl leading-relaxed md:text-7xl  font-lora text-pink-50 ">
+            Healthy Mind
+            <br />
+            <span className="my-10 md:pl-40">Healthy Living</span>{" "}
+          </h1>
+          <p className="max-w-[40rem] font-sans text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quam
+            iure, voluptates asperiores quisquam provident hic recusandae natus,
+            atque eaque sunt minima?
+          </p>
+          <button className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded">
+            Book Appointment
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
