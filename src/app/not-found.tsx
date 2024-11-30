a81afe1ed527d921c7f2f2d668af148d2ea546e0
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-11-30 22:32:16 +0530

    not found pg

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="bg-teal-950  flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">Page Not Found</h1>
       <p className="mt-2 text-gray-500">
-        Sorry, we couldn’t find the page you’re looking for.
+        Sorry, we couldn not find the page you are looking for.
       </p>
-      <a href="/" className="mt-4 text-blue-500 underline">
-        Go back home
-      </a>
+      <Link href="/">
+        <h1>Go back Home</h1>
+      </Link>
     </div>
   );
 }
