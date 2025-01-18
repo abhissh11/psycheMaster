b84341f2960cac878f93f41ce5db7710a4620832
dev-abhishekkr <dev.abhishekkr@gmail.com>
2025-01-18 15:29:40 +0530

    revamp of contact page

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -1,4 +1,4 @@
-import { Instagram } from "lucide-react";
+import { Instagram, Mail } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { Icon } from "@iconify/react";
@@ -9,42 +9,79 @@ export default function ContactComponent() {
   return (
     <div className="px-4 md:px-10 lg:px-20 py-10 md:py-20 bg-base">
       <div className="flex md:flex-row flex-col gap-10 w-full justify-between border bg-chase rounded-3xl border-purple-900">
-        <div className="flex flex-col gap-10 justify-center items-center w-full md:w-1/2 py-6">
+        <div className="flex flex-col gap-10 justify-start items-center w-full md:w-1/2 py-6">
           <h1 className="text-3xl font-bold font-mono text-white mt-6 md:mt-2">
             GET IN TOUCH WITH US
           </h1>
-          <Image
-            src="/images/psychemasters-card.jpg"
-            height={200}
-            width={200}
-            alt="PsycheMaster's Logo"
-            className="rounded-2xl shadow-md shadow-gray-900 hover:shadow-xl transition-all hover:shadow-black"
-          />
-          <div className="flex gap-4 items-center py-4">
-            <Link
-              href="https://www.instagram.com/psychemasters/"
-              target="_blank"
-            >
-              <button className="animate-pulse flex items-center gap-2 text-base font-medium font-sans bg-gradient-to-tr from-purple-700 to-pink-600 px-4 py-2 rounded  text-white ">
-                <span>
-                  <Instagram size={28} />
-                </span>{" "}
-                Instagram
-              </button>
-            </Link>
-            <p className="text-lg font-medium text-white font-sans">or</p>
-            <Link
-              href="https://chat.whatsapp.com/KP90vTFesp7J2wjvIDMzpX"
-              target="_blank"
-            >
-              <button className="animate-pulse flex items-center gap-2 text-base font-medium px-4 py-2 rounded bg-green-500 font-sans text-white">
-                <span>
-                  {" "}
-                  <Icon icon="uim:whatsapp" className="w-7 h-7" />
-                </span>{" "}
-                WhatsApp
-              </button>
-            </Link>
+          <div className="flex flex-col">
+            <div className="flex gap-2 items-center mb-6">
+              <Mail
+                size={44}
+                className="text-2xl text-white bg-blue-600 p-2 rounded"
+              />
+              <div className="">
+                <p className="text-xl font-semibold text-gray-200">
+                  Email us at
+                </p>
+                <Link
+                  href="#"
+                  target="_blank"
+                  className="text-sm font-medium text-white"
+                >
+                  psychemastersindia@gmail.com
+                </Link>
+              </div>
+            </div>
+            <div className="flex gap-2 items-center mb-6">
+              <span>
+                {" "}
+                <Icon
+                  icon="uim:whatsapp"
+                  className="w-12 h-12 rounded text-white bg-blue-600 p-2"
+                />
+              </span>{" "}
+              <div className="">
+                <p className="text-xl font-semibold text-gray-200">
+                  Join our WhatsApp community
+                </p>
+                <Link
+                  href="#"
+                  target="_blank"
+                  className="text-sm font-medium text-white"
+                >
+                  WhatsApp
+                </Link>
+              </div>
+            </div>
+            <div className="flex gap-2 items-center mb-6">
+              <span>
+                <Instagram
+                  size={46}
+                  className="text-white p-2 bg-blue-600 text-2xl rounded"
+                />
+              </span>{" "}
+              <div className="">
+                <p className="text-xl font-semibold text-gray-200">
+                  Follow us on Instagram
+                </p>
+                <Link
+                  href="#"
+                  target="_blank"
+                  className="text-sm font-medium text-white"
+                >
+                  Instagram
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 p-2 rounded-full">
+            <Image
+              src="/images/master.png"
+              height={120}
+              width={120}
+              alt="PsycheMaster's Logo"
+              className="rounded-full shadow-md shadow-purple-800 hover:shadow-xl hover:shadow-purple-800 transition-all"
+            />
           </div>
         </div>
 
@@ -56,4 +93,19 @@ export default function ContactComponent() {
       </div>
     </div>
   );
+}
+
+{
+  /* <Image
+src="/images/psychemasters-card.jpg"
+height={200}
+width={200}
+alt="PsycheMaster's Logo"
+className="rounded-2xl shadow-md shadow-gray-900 hover:shadow-xl transition-all hover:shadow-black"
+/>
+<div className="flex gap-4 items-center py-4">
+<Link
+  href="https://www.instagram.com/psychemasters/"
+  target="_blank"
+> */
 }
